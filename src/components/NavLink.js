358ac0b1e747368c9e4d74352b08836ec4940968
Josef Šupka <josef.supka@gmail.com>
2020-05-15 05:47:03 +0200

    Change to simple static site
    Previous Create React App has been “serialized” to pure HTML, CSS, and
    JavaScript. This is mainly to remove unnecessary abstraction layers and
    deployment pipeline while fixing immediate issues with site’s
    “index-ability” and “share-ability”.
    
    There are also many minor changes which seemed reasonable given the
    amount code changes:
    
    * Using Google Analytics script directly instead of gtag.js. We are not
    using other Google marketing tools, so was triggering yet another
    request.
    
    * Fix Google Analytics configuration so we don’t have to ask for
      permission to process private information. Previously, we used “store”
      option which seems to no longer exists. Now, we “cookieExpires” which
      does the same things – creates pseudonymous cookie for the duration of
      visitor’s session (instead of default two years).
    
    * Make the site work well without JavaScript and improve accessibility.
    
      + Keep priorities expanded if JavaScript isn’t available.
    
      + Set language to Czech.
    
      + Add additional semantic section and grouping elements for main,
        header and aside parts of the page.
    
      + Linked labels to respective form fields.
    
      + Set autofill attributes on form field.
    
      + Remove useless or inappropriate (therefore confusing) ARIA roles.
    
      + Set title on inline SVGs to provide textual fallback.
    
    * Don’t provide manifest nor service worker since this is pretty much
      document-oriented website.
    
    * Create new style rules to display expand and collapse icons for policy
      priorities. This is therefore no longer handled by JavaScript, but by
      CSS. We still use collapse component from Bootstrap.
    
    * Use Lato latin web variant (“LatoLatinWeb”) which covers complete
      Czech alphabet but it’s smaller than complete Lato web fonts.
    
    * Update to Bootstrap 4.5 and append use custom minified build (with
      jQuery slim build). The custom build reflects our customization:
    
      + primary: #6e1646
      + font-family base: Lato (see above)
      + headings-color: primary (see above)
    
    * Update to Font Awesome 5.13, append “svg-with-js” minified style and
      use inline SVGs via sprite. However, we’ve created a custom sprint
      with only a handful of handpicked icons:
    
      + facebook
      + twitter
      + instagram
      + minus
      + plus
    
      We don’t use Font Awesome JavaScript code.
    
    * Logo of our movement in SVG.
    
    * Displayed logo with no left-padding to align with the left content
      border.
    
    * Update additionally linked logos (favicon, Open Graph, touch icon).
    
    * Set title based on displayed page
    
    * Add canonical link element and Open Graph meta elements
    
    * As mentioned above, we concatenate our small custom CSS with selected
      Lato web font, Bootstrap custom build and Font Awesome SVG style to
      one CSS file.
    
    * Similarly, we concatenate small custom JS (only sets document
      element’s class to differentiate JS-enabled user-agent in CSS) with
      jQuery slim build and Bootstrap build to one JS file.
    
    Co-authored-by: Josef Šupka <josef.supka@gmail.com>

--- src/components/NavLink.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { NavLink as RRNavLink } from 'react-router-dom'
-import { NavLink as RSNavLink } from 'reactstrap'
-
-function NavLink(props) {
-  return <RSNavLink tag={RRNavLink} activeClassName="active" {...props} />
-}
-
-export default NavLink
